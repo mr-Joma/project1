b7c3b70f5b75b56beef15eb46d77365fb21684be
mr-Joma <jomaturgunbekov@gmail.com>
2026-04-05 23:14:05 +0600

    HW2

--- main.py
+++ main.py
@@ -1,51 +1,51 @@
-import flet as ft
+# import flet as ft
 
-def main_page (page: ft.Page):
-    page.title = "Мое первое приложение!!"
-    page.theme_mode = ft.ThemeMode.LIGHT
+# def main_page (page: ft.Page):
+#     page.title = "Мое первое приложение!!"
+#     page.theme_mode = ft.ThemeMode.LIGHT
     
-    text_hello = ft.Text(value="Hello World!!")
+#     text_hello = ft.Text(value="Hello World!!")
     
-    greeting_history = []
+#     greeting_history = []
     
-    history_text = ft.Text(value="История приветствий: ")
+#     history_text = ft.Text(value="История приветствий: ")
     
-# Поменять значение 
-    text_hello.value = "Hello GEEKS!!"
+# # Поменять значение 
+#     text_hello.value = "Hello GEEKS!!"
 
     
-    def on_butt_clic(_):
-        print(name_input.value)
-        name = name_input.value
+#     def on_butt_clic(_):
+#         print(name_input.value)
+#         name = name_input.value
         
-        if name:
-            text_hello.color = None
-            text_hello.value = f"Hello {name}"
+#         if name:
+#             text_hello.color = None
+#             text_hello.value = f"Hello {name}"
             
-            name_input.value = None
+#             name_input.value = None
             
-            greeting_history.append(name)
+#             greeting_history.append(name)
             
             
-            history_text.value = "История приветствий:\n" + '\n'.join(greeting_history)
-        else:
-            text_hello.color = ft.Colors.RED
-            text_hello.value = "Введите имя"
-            print("Ничегосне ввели!!")
-        page.update()
+#             history_text.value = "История приветствий:\n" + '\n'.join(greeting_history)
+#         else:
+#             text_hello.color = ft.Colors.RED
+#             text_hello.value = "Введите имя"
+#             print("Ничегосне ввели!!")
+#         page.update()
             
     
     
-    name_input = ft.TextField(on_submit=on_butt_clic, label="Введите имя", expand=True)
-    elevated_button = ft.ElevatedButton('send', icon=ft.Icons.SEND, color=ft.Colors.RED,
-                                        icon_color=ft.Colors.GREEN, on_click=on_butt_clic)
+#     name_input = ft.TextField(on_submit=on_butt_clic, label="Введите имя", expand=True)
+#     elevated_button = ft.ElevatedButton('send', icon=ft.Icons.SEND, color=ft.Colors.RED,
+#                                         icon_color=ft.Colors.GREEN, on_click=on_butt_clic)
     
-    main_object = ft.Row([name_input, elevated_button])
+#     main_object = ft.Row([name_input, elevated_button])
     
     
-    page.add(text_hello, main_object, history_text)
+#     page.add(text_hello, main_object, history_text)
 
-ft.app(main_page)
+# ft.app(main_page)
 
 
 # HW1
@@ -67,3 +67,15 @@ ft.app(main_page)
 #     )
 
 # ft.app(main)
+
+
+
+# HW2
+
+import datetime
+
+name = input("Введите имя: ")
+
+now = datetime.datetime.now()
+
+print(f"{now.year}:{now.month}:{now.day} - {now.hour}:{now.minute}:{now.second} - Привет, {name}!")
