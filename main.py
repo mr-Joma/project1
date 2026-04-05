32b37f9259b3483332ca61e9cbbd67ca9c55e5fe
mr-Joma <jomaturgunbekov@gmail.com>
2026-04-05 23:01:58 +0600

    main

--- main.py
+++ main.py
@@ -1,41 +1,69 @@
-# import flet as ft
+import flet as ft
 
-# def main_page (page: ft.Page):
-#     page.title = "Мое первое приложение!!"
-#     page.theme_mode = ft.ThemeMode.LIGHT
+def main_page (page: ft.Page):
+    page.title = "Мое первое приложение!!"
+    page.theme_mode = ft.ThemeMode.LIGHT
     
-#     text_hello = ft.Text(value="Hello World!!")
+    text_hello = ft.Text(value="Hello World!!")
     
-# # Поменять значение 
-#     text_hello.value = "Hello GEEKS!!"
+    greeting_history = []
+    
+    history_text = ft.Text(value="История приветствий: ")
+    
+# Поменять значение 
+    text_hello.value = "Hello GEEKS!!"
 
-#     name_input = ft.TextField()
     
-#     elevated_button = ft.ElevatedButton('send', icon=ft.Icons.SEND, color=ft.Colors.RED, icon_color=ft.Colors.GREEN)
-#     text_button = ft.TextButton('send')
-#     icon_button = ft.IconButton(icon=ft.Icons.SEND)
+    def on_butt_clic(_):
+        print(name_input.value)
+        name = name_input.value
+        
+        if name:
+            text_hello.color = None
+            text_hello.value = f"Hello {name}"
+            
+            name_input.value = None
+            
+            greeting_history.append(name)
+            
+            
+            history_text.value = "История приветствий:\n" + '\n'.join(greeting_history)
+        else:
+            text_hello.color = ft.Colors.RED
+            text_hello.value = "Введите имя"
+            print("Ничегосне ввели!!")
+        page.update()
+            
     
-#     page.add(text_hello, name_input, elevated_button, text_button,icon_button)
+    
+    name_input = ft.TextField(on_submit=on_butt_clic, label="Введите имя", expand=True)
+    elevated_button = ft.ElevatedButton('send', icon=ft.Icons.SEND, color=ft.Colors.RED,
+                                        icon_color=ft.Colors.GREEN, on_click=on_butt_clic)
+    
+    main_object = ft.Row([name_input, elevated_button])
+    
+    
+    page.add(text_hello, main_object, history_text)
 
-# ft.app(main_page)
+ft.app(main_page)
 
 
 # HW1
-import flet as ft
+# import flet as ft
 
-def main(page: ft.Page):
-    count = 0
-    text_hello = ft.Text("Нажато: 0 раз")
+# def main(page: ft.Page):
+#     count = 0
+#     text_hello = ft.Text("Нажато: 0 раз")
 
-    def click(e):
-        nonlocal count
-        count += 1
-        text_hello.value = f"Нажато: {count} раз"
-        page.update()
+#     def click(e):
+#         nonlocal count
+#         count += 1
+#         text_hello.value = f"Нажато: {count} раз"
+#         page.update()
 
-    page.add(
-        text_hello,
-        ft.ElevatedButton("Нажми", on_click=click)
-    )
+#     page.add(
+#         text_hello,
+#         ft.ElevatedButton("Нажми", on_click=click)
+#     )
 
-ft.app(main)
+# ft.app(main)
